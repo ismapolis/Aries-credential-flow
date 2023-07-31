21944b886899e18796d874756248c708b9ef9bf8
Ismael Illán García <iillan@iti.es>
2023-07-31 12:51:48 +0200

    faltaban exportaciones

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,19 @@
-import { IDataStore, IDataStoreORM } from "./data-store/index.js";
+import {
+  IDataStore,
+  IDataStoreORM,
+  DataStore,
+  DataStoreORM,
+} from "./data-store/index.js";
 import { ICredentialFlow } from "./types/ICredentialFlow.js";
 import { CredentialFlow } from "./credentialFlow.js";
 import * as types from "./types/types.js";
 
-export { ICredentialFlow, IDataStore, IDataStoreORM, CredentialFlow, types };
+export {
+  ICredentialFlow,
+  CredentialFlow,
+  IDataStore,
+  DataStore,
+  IDataStoreORM,
+  DataStoreORM,
+  types,
+};
